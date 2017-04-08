56283d00a7a8bf224d0fa42952da3a0c108b0171
Trent Marshall <Trent@Sarahs-MacBook-Air-2.local>
2017-04-08 01:32:31 -0400

    v1.3

--- liri.js
+++ liri.js
@@ -10,6 +10,7 @@ var keys = require("./keys.js");
 var inputCommand = process.argv[2];
 var title = process.argv[3];
 
+//switch statement to test the condition of the inputCommand
 switch(inputCommand) {
 	case "my-tweets":
 		myTweets();
@@ -23,9 +24,13 @@ switch(inputCommand) {
 	case "do-what-it-says":
 		doWhatItSays();
 		break;
+	case undefined:
+		console.log("Try again!");
+		break;
 }
-
+//designate tweets function
 function myTweets() {
+	//setup client with my keys stored in keys.js
 	var client = new twitter({
 		consumer_key: keys.twitterKeys.consumer_key,
 		consumer_secret: keys.twitterKeys.consumer_secret,
@@ -36,37 +41,52 @@ function myTweets() {
 		var params = {screen_name: 'alecbaldwin'};
 		client.get('statuses/user_timeline', params, function(error, tweets, response) {
   			if (!error) {
-  				for (var i = 0; i < 20; i++){
-  					var tweet = tweets[i].text + "\r\n" + "Tweeted on: " + tweets[i].created_at + "\r\n" + "===================================" + "\r\n";
-    				// console.log(tweets[i].text + "\r\n" + "Tweeted on: " + tweets[i].created_at + "\r\n" + "===================================" + "\r\n");
+  				for (var i = 0; i < tweets.length; i++){
+  					//display tweets index starting number as 1 and not 0
+  					// Tweet #0 makes no sense!
+  					var tweetNum = i + 1;
+  					var tweet = "Tweet #" + tweetNum + ": " + "\r\n" + 
+  					"===================================" + "\r\n" + 
+  					tweets[i].text + "\r\n" + "Tweeted on: " + 
+  					tweets[i].created_at + "\r\n" + 
+  					"===================================" + "\r\n";
     				console.log(tweet);
-    				writeText(tweet);
+    				log(tweet);
     			}
+  			}
+  			else {
+  				console.log(error);
   			}
 		});
 }
 
+//pass title from node input to spotify search engine
 function spotifySearch(title) {
+	//if there title is undefined, default to the sign
 	if (!title) {
    		title = "ace of base - the sign";
-   		console.log(title);
    	}
    	//search spotify based on user input
 	spotify.search({ type: 'track', query: title}, function(err, data) {
-   			
+   		//if we receive an error, log it	
     	if ( err ) {
         	console.log('Error occurred: ' + err);
         	return;
    		}
    		else {
+   			//store the object into a shorter variable for readability
    			var songData = data.tracks.items;
+   			//print one result from spotify
    			for (var i = 0; i < 1; i++){
-   				var song = "Artist: " + songData[i].artists[0].name + "\r\n" + 
+   				//build song info screen
+   				var song = "===================================" + "\r\n" +
+   							"Artist: " + songData[i].artists[0].name + "\r\n" + 
   							"Title: " + songData[i].name + "\r\n" +
   							"Album: " + songData[i].album.name + "\r\n" +
-  							"Preview Link: " + songData[i].preview_url;
+  							"Preview Link: " + songData[i].preview_url + "\r\n" +
+  							"===================================";
    							console.log(song);
-   							writeText("\r\n ////////// \r\n" + song + "\r\n //////////");
+   							log("\r\n %%%%%%%%%% \r\n" + song + "\r\n %%%%%%%%%%");
    			}
    		}
    	});
@@ -74,6 +94,7 @@ function spotifySearch(title) {
 
 //OMDB search function
 function movieSearch(title) {
+	//if title is undefined, default to Mr. Nobody
 	if (!title) {
    				title = "Mr. Nobody";
    	}
@@ -82,17 +103,20 @@ function movieSearch(title) {
  		// If there were no errors
 		if (!error && response.statusCode === 200) {
 			//store OMDB JSON data to variable
-  			var movieInfo = "Title: " + JSON.parse(body).Title + "\r\n" + 
+			//JSON.parse seems to work best here
+  			var movieInfo = "===================================" + "\r\n" +
+  							"Title: " + JSON.parse(body).Title + "\r\n" + 
   							"Year: " + JSON.parse(body).Year + "\r\n" +
-  							"Rating: " + JSON.parse(body).imdbRating + "\r\n" +
+  							"IMDB Rating: " + JSON.parse(body).imdbRating + "\r\n" +
   							"Country: " + JSON.parse(body).Country +  "\r\n" +
   							"Language: " +JSON.parse(body).Language + "\r\n" +
   							"Plot: " + JSON.parse(body).Plot + "\r\n" +
   							"Actors: " +JSON.parse(body).Actors + "\r\n" +
-  							"Rotten Tomatoes Rating: " +JSON.parse(body).Ratings[1].Value;
+  							"Rotten Tomatoes Rating: " +JSON.parse(body).Ratings[1].Value + "\r\n" + 
+  							"===================================";
   			//log to screen
   			console.log(movieInfo);
-  			writeText("\r\n ////////// \r\n" + movieInfo + "\r\n //////////");
+  			log("\r\n %%%%%%%%%% \r\n" + movieInfo + "\r\n %%%%%%%%%%");
  		}
 	});
 }
@@ -101,17 +125,15 @@ function movieSearch(title) {
 function doWhatItSays() {
 	fs.readFile('random.txt', 'utf8', function(error, data){
 		var dataArr = data.split(",");
-		console.log(dataArr[1]);
 		spotifySearch(dataArr[1]);
 	})
 }
+
 //BONUS append every succesful query to log.txt
-function writeText(input) {
+function log(input) {
 	fs.appendFile('log.txt', input, function(err) {
 		if(err) {
 			console.log(err);
 		}
 	});
 }
-
-
